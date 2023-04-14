c5a686a53261d56a99ca3a1f5e853d4f567b8eab
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-04-14 14:00:06 +0200

    add tests for course list and setup test for home

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
 
@@ -16,16 +16,34 @@ import {click} from '../common/test-utils';
 
 
 
+
 describe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
   let component:HomeComponent;
   let el: DebugElement;
 
-  beforeEach((() => {
+  beforeEach (waitForAsync((() => {
 
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
 
-  }));
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        {provide: CoursesService, useValue: coursesServiceSpy}
+      ]
+    }).compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(HomeComponent)
+      component = fixture.componentInstance;
+      el = fixture.debugElement;
+    })
+
+    })
+  ));
 
   it("should create the component", () => {
 
