5b29d89712fc5756ecf2334e0d2f37d3a2f46213
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-04-17 10:04:51 +0200

    changed get to inject

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -45,7 +45,7 @@ describe('HomeComponent', () => {
       fixture = TestBed.createComponent(HomeComponent)
       component = fixture.componentInstance;
       el = fixture.debugElement;
-      coursesService = TestBed.get(CoursesService)
+      coursesService = TestBed.inject(CoursesService)
     })
 
     })
